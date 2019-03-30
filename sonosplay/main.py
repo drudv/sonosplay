cf331d4552817a0897fee24933dcbd3eb017e2ac
Dmitry Druganov <drud@drud.cz>
2019-03-30 22:04:10 +0100

    Serve and play mp3

--- sonosplay/main.py
+++ sonosplay/main.py
@@ -1,9 +1,19 @@
 import sys
+import argparse
 from .streamer import Streamer
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='Media file name to play')
+    parser.add_argument('--port', type=int, help='Port number to listen on')
+    parser.add_argument('--ip', help='IP address to bind to')
+    parser.add_argument('--player', help='Sonos player name')
+    args = parser.parse_args()
     streamer = Streamer()
-    streamer.run()
+    streamer.run(media_file=args.file,
+                 predefined_port=args.port,
+                 predefined_ip_addr=args.ip,
+                 predefined_player_name=args.player)
 
 if __name__ == '__main__':
     main()
--- sonosplay/streamer.py
+++ sonosplay/streamer.py
@@ -1,13 +1,47 @@
+import os
+import soco
 import socket
+import sys
+import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import TCPServer
 
 START_PORT = 9000
 END_PORT = 9999
+STREAM_PATH = '/stream.mp3'  # Sonos player wants this '.mp3' extension
 
-class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        return BaseHTTPRequestHandler.do_GET(self)
+
+def create_handler(media_file):
+    class CustomRequestHandler(BaseHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super(CustomRequestHandler, self).__init__(*args, **kwargs)
+
+        def do_GET(self):
+            if self.path == STREAM_PATH:
+                try:
+                    with open(media_file, 'rb') as file:
+                        basename = os.path.basename(media_file)
+                        stat = os.fstat(file.fileno())
+                        self.send_response(200)
+                        self.send_header('Content-Length', str(stat[6]))
+                        self.send_header(
+                            'Content-Type',
+                            'audio/mpeg')
+                        self.send_header(
+                            'Content-Disposition',
+                            'attachment; filename="%s"' % basename)
+                        self.end_headers()
+                        self.wfile.write(file.read())
+                except socket.error as error:
+                    print('Connection is closed by peer')
+                    pass
+                except IOError as error:
+                    self.send_response(500)
+                    print('I/O error: %s' % (str(error)))
+                return
+            self.send_error(404)
+    return CustomRequestHandler
+
 
 class Streamer:
     def is_port_in_use(self, port):
@@ -31,10 +65,32 @@ class Streamer:
             raise Exception('Cannot detect IP address to use')
         return ip_addr
 
-    def run(self, predefined_port=None, predefined_ip_addr=None):
+    def get_player(self, predefined_player_name=None):
+        players = list(soco.discover())
+        if len(players) == 0:
+            raise Exception('Cannot find any Sonos player')
+        if predefined_player_name is None:
+            return players[0]
+        try:
+            return next(p for p in players
+                        if p.player_name == predefined_player_name)
+        except StopIteration:
+            raise Exception("Player %s doesn't exist" % predefined_player_name)
+
+    def run(self,
+            media_file,
+            predefined_port=None,
+            predefined_ip_addr=None,
+            predefined_player_name=None):
         port = predefined_port or self.find_free_port()
         ip_addr = predefined_ip_addr or self.detect_ip_addr()
-        with socketserver.TCPServer((ip_addr, port), RequestHandler) as httpd:
-            print("Listening to %s:%d" % (ip_addr, port))
-            httpd.serve_forever()
-
+        player = self.get_player(predefined_player_name)
+        Handler = create_handler(media_file=media_file)
+        with HTTPServer((ip_addr, port), Handler) as httpd:
+            stream_url = 'http://%s:%s%s' % (ip_addr, port, STREAM_PATH)
+            print('Streaming on %s' % stream_url)
+            thread = threading.Thread(target=httpd.serve_forever)
+            thread.daemon = True
+            thread.start()
+            player.play_uri(stream_url)
+            thread.join()
