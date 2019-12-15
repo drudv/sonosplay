a5712d4bb5b7ab4527d0e12b86aafcadc630ef84
Dmitry Druganov <drud@drud.cz>
2019-12-15 19:23:47 +0100

    update SoCo dependency to fix discovery issue

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='sonosplay',
     version='0.1.0',
     packages=['sonosplay'],
-    install_requires=['soco'],
+    install_requires=['soco>=0.18.1'],
     entry_points={
         'console_scripts': [
             'sonosplay = sonosplay.main:main'
